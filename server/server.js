ad2d94598516bb837d5200b82f6273366dd111be
mrko147 <95499236+mrko147@users.noreply.github.com>
2023-01-10 17:13:08 +0000

    fix the subject so that the name appear

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ app.use(cors());
 
 const port = 3000;
 const lessons =  [
-    {" topic": "math", "location": "Hendon", "price" : 100},
-    {" topic": "math", "location": "Colindale", "price" : 80},
-    {" topic": "math", "location": "Brent Cross", "price" : 90},
-    {" topic": "math", "location": "Golders Green", "price" : 120},
+    {"topic": "math", "location": "Hendon", "price" : 100},
+    {"topic": "math", "location": "Colindale", "price" : 80},
+    {"topic": "math", "location": "Brent Cross", "price" : 90},
+    {"topic": "math", "location": "Golders Green", "price" : 120},
 ];
 const user = {"email": "user@email.com", "password": "mypassword"};
 
